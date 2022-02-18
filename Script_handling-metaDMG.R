b4daa23086301929844b795dd967c4cd9d76c35e
GiuliaZ538 <giulia.zampirolo@gmail.com>
2022-02-18 12:42:23 +0100

    Additional codes

--- Script_handling-metaDMG.R
+++ Script_handling-metaDMG.R
@@ -25,7 +25,30 @@ names(df)[names(df) == 'new'] <- 'Depth'
 df$new <- context_data$Date[match(df$sample, context_data$Sample_ID)]
 names(df)[names(df) == 'new'] <- 'Date'
 
-#set all NAs as zeros
-df$Date[is.na(df$Date)] <- "Control"
+##Adding factor for filtering (to keep updated based on the variable used)
+output1_data <- df
 
+output2_data <- output1_data %>%   # overwriting our data frame
+  mutate(lambda_LR_new =   # creating our new column
+           ifelse(lambda_LR > 10, "LR>10", "LR<10"))
 
+##Adding grouping factor on Kingdoms
+output3_data <- output2_data %>% 
+  mutate(Kingdom =   # creating our new column
+           case_when(grepl("Viridiplantae", tax_path) ~ "Viridiplantae",
+                     grepl("2:Bacteria:",tax_path) ~ "Bacteria",
+                     grepl("Metazoa",tax_path) ~ "Metazoa",
+                     grepl("Fungi", tax_path) ~ "Fungi",
+                     grepl("Archaea", tax_path) ~ "Archaea"))
+
+#set NAs
+output3_data[is.na(output3_data)] <- "Control"
+output3_data$Kingdom[output3_data$Kingdom == "Control"] <- "Indet"
+
+#Subset dataframe to show only genus, filtering reads
+subset_genus <- output3_data %>% filter(D_max > 0.000, N_reads > 20, grepl("\\bgenus\\b", tax_rank))
+
+#Other subsets for parsing reads, for ex. plants, animals, filtering D_max, lambda_LR.
+subset1 <- output3_data %>% filter(D_max > 0.05, lambda_LR > 10, grepl("Viridiplantae", tax_path))
+subset2 <- output3_data %>% filter(D_max > 0.02, N_reads > 100, lambda_LR > 5, grepl("Metazoa", tax_path))
+subset3 <- output3_data %>% filter (N_reads > 50, grepl("Bacteria", Kingdom))
