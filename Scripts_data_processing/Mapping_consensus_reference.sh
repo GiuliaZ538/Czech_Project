321ddd2578f12db551047ee165f1bc8292bfaeda
GiuliaZ538 <giulia.zampirolo@gmail.com>
2023-02-14 19:11:36 +0100

    update

--- Scripts_data_processing/Mapping_consensus_reference.sh
+++ Scripts_data_processing/Mapping_consensus_reference.sh
@@ -1,4 +1,4 @@
-#####Mapping damaged reads to consensus reference of mitogenomes########
+#####Read extraction by readID and consensus fasta files
 
 ##Tools
 bowtie2-build, bowtie2-align (v. 2.3.2)
@@ -6,18 +6,17 @@ mafft (v7.427)
 raxml-ng (v. 0.8.1 BETA)
 angsd (v. 0.928)
 Geneious Prime (v. 2021.2.2 )
-Jalview (v. 2.11.0)
 
-1. ###Create fastq file with taxa of interest
+1. ##Create fastq file with taxa of interest
 
 #Print sample list based on name files
 ll *lca | awk '{print $}' > sample.list
 ll *lca | cut -f2 -d: | cut -f2 -d " " > sample.list
 
-##Taxalist
-nano name_genus
+#Taxalist
+nano name_genus or name_family
 
-##Create txt file with all readIDs
+#Create txt file with all readIDs
 while read -r line
 do
 arr=($line)
@@ -27,7 +26,7 @@ echo $lib
 cat taxa.list | parallel -j20 "grep {} $lib | cut -f1,2,3,4,5,6,7 -d: > $lib.{}.readID.txt"
 done < sample.list
 
-##Remove file were not found name_genus
+#Remove file were not found name_genus
 wc -l *.readID.txt| awk '$1 == 0' | awk '{print $2}' > rm.list
 cat rm.list | parallel -j20 "rm {}"
 
@@ -46,7 +45,7 @@ seqtk subseq fq_link/*$bname2*rmdup.fq $bname > $bname1 &
   #seqtk seq -a $bname1 > $bname1.fa ##NOTHING IN IT conversion into fasta didnt work
   done
 
-##Crete symbolic links of fastq files to new folder
+#Create symbolic links of fastq files to new folder
 for file in PATHfqfiles_extracted/*.readID.fq
 do
 bname=$(basename $file)
@@ -55,9 +54,7 @@ ln -s $file PATH_new_folder
 done
 
 2. ##Prepare fasta reference files
-#Check duplicates with Jalview software and remove them if any.
-
-###Be sure to have removed tab and , " , ' , : from sequence!
+#Be sure to have removed tab and , " , ' , : from sequence!
 sed "s/:/_/g" in.fa > out.fa
 sed "s/ /_/g" in.fa > out.fa
 sed "s/;/_/g" in.fa > out.fa 
@@ -68,8 +65,7 @@ sed "s/)/./g" in.fa > out.fa
 #Alignment references
 mafft --thread 40 --leavegappyregion in_to_align.fa > out_aligned.fa
 
-#if needed a consensus of the references
-Make Consensus Sequence with Geneious software: use the most common base, and ignore gaps
+#if needed a consensus of the references(Geneious software: the most common base rule, and ignore gaps)
 
 #Checking tree of of the reference
 raxml-ng --search1 --msa in_aligned.fa --model GTR+G --prefix Out_name --threads 2 --seed 2
@@ -82,41 +78,26 @@ bowtie2-inspect -s in.fa #you can inspect file the Database
 3. ##Mapping readID.fq files to the reference database 
 for file in *.fq
 do
-db=OVISclean2c_db
-fasta=OVISclean2c_aligned.fa
+db=Reference_db
+fasta=Reference_aligned.fa
 bowtie2 -x $db -U $file --threads 60 --no-unal | samtools view -bS -> $file.bam
 angsd -doFasta 2 -doCounts 1  -i $file.bam -out $file.consensus.fa
 done
-----------------------
-3.1 # for making individual consensus split the fastq from each genome into one read fastq
-for file in *.fq
-do
-split -l 4 $file $file.prefix.fq &
-  done
 
-# then map all individual reads against the reference
-for file in *prefix*
-  do
-db=in_db
-fasta=in_aligned.fa
-bowtie2 -x $db -U $file --threads 60 --no-unal | samtools view -bS -> $file_split.bam
-angsd -doFasta 2 -doCounts 1  -i $file_split.bam -out $file.consensus.fa
-done
--------------------------
 4. ##Bamcov outputs
 for file in *.bam
 do
 samtools sort -O BAM -o sort_$file $file
 done
 
-##To get bamcov histograms
+#To get bamcov histograms
 for file in sort*.fq.bam
 do
 echo $file
 bamcov -m -w0 $file | paste > bamcov_$file.txt 
 done
 
-##To get Bamcov tables
+#To get Bamcov tables
 for file in sort_*.bam
 do
 bname=$(basename $file)
@@ -124,13 +105,13 @@ echo $bname
 bamcov -Q 30 $file | paste > bamcov_table30filter_$file.txt
 done
 
-5. ####GET the consensus reads 
+5. ##GeT the consensus reads 
 for file in *.bam
 do
 angsd -doFasta 2 -doCounts 1 -i $file -out $file.consensus.fa
 done
 
-#GUNZIP MANY READS
+#Gunzip
 for file in *.consensus.fa.fa.gz
 do
 bname=$(basename $file)
@@ -138,15 +119,15 @@ echo $bname
 gunzip $file 
 done
 
-##DELETE SIZE 0
-for file in /willerslev/users-shared/science-snm-willerslev-mxd270/MetaDMG_CzechALL/LCA/VM/Bovinae/*.consensus.fa.fa
+#Delete Size 0
+for file in *.consensus.fa.fa
 do
 bname=$(basename $file)
 echo $bname
 find $file -size 0 -delete
 done
 
-##Rename headers consensus reads
+#Rename headers consensus reads
 for infile in *.fa.fa
 do
 bname=$(basename $infile)
@@ -155,13 +136,4 @@ echo $bname
 echo $bname2
 /willerslev/software/bbmap/rename.sh in=$bname out=$bname.fasta prefix=$bname2
 done
-
-6. ###Alignment and Tree building/CHECK BEAST
-cat *consensus.fasta Ref_not_aligned.fa > Allref_mycons.fa
-
-mafft --thread 40 --leavegappyregion Ref_Cons_reads.fa > Ref_Cons_reads_aligned.fa
-
-raxml-ng --search1 --msa Ref_Cons_reads_aligned.fa --model GTR+G --prefix OVis_CR_Cybt_ALL --threads 2 --seed 2 
-
-###REFERENCES Tools
 
