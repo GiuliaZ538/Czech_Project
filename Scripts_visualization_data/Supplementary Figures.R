fab081e4d0ee95bf9142dd4d42b61f1e4761e6a0
GiuliaZ538 <giulia.zampirolo@gmail.com>
2023-02-14 16:56:44 +0100

    update2

--- Scripts_visualization_data/Supplementary Figures.R
+++ Scripts_visualization_data/Supplementary Figures.R
@@ -1,5 +1,47 @@
-###Supplementary Figures 
+###Supplementary Figures 3
+######metaDMG_make wide table - abundance indexes
+sample2 <- subset_dataset$sample
+dmax <- subset_dataset$Bayesian_D_max
+taxid2 <- subset_dataset$tax_name
+meanl <- subset_dataset$mean_L
 
+subset_wide1 <- data.frame(taxid2, sample2, dmax)
+subset_wide2 <- data.frame(taxid2, sample2, meanl)
+subset_wide_filtered1 <- subset_wide1 %>% filter(N_reads > 1000)
+subset_wide_filtered2 <- subset_wide2 %>% filter(N_reads > 1000)
+
+#Scatter plot + standard deviation D-max
+pd <- position_dodge(0.5)
+p11 <- subset_wide_filtered1 %>%
+  mutate(sample = fct_relevel(sample,
+                              "VM-19","VM-17","VM-15","VM-14"))%>%
+  ggplot(aes(x=Bayesian_D_max, y=sample, colour=tax_name)) + 
+  geom_errorbar(aes(xmin=Bayesian_D_max-Bayesian_D_max_std, xmax=Bayesian_D_max+Bayesian_D_max_std, colour=tax_name, group=tax_name),position=pd) +
+  geom_point(size = 2.2, position=pd)+
+  scale_colour_manual(values=c("#006400", "#00CED1", "#EEC900", "#8B4500", "#66CD00"))+
+  #scale_x_continuous(breaks = seq(0,0.15, by=0.05), limits = c(0,0.15))+
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + ggtitle("Bayesian D-max by depth") + xlab ("Bayesian D-max")+ ylab ("Sample") # labs(colour="Tax")
+p11
+
+#Scatter plot + standard deviation Mean length
+pd <- position_dodge(0.5)
+p12 <- subset_wide_filtered2 %>%
+  mutate(sample = fct_relevel(sample,
+                              "VM-19","VM-17","VM-15","VM-14"))%>%
+  ggplot(aes(x=mean_L, y=sample, colour=tax_name)) + 
+  geom_errorbar(aes(xmin=mean_L-std_L, xmax=mean_L+std_L, colour=tax_name, group=tax_name),position=pd) +
+  geom_point(size = 2.2, position=pd)+
+  scale_colour_manual(values=c("#006400", "#00CED1", "#EEC900", "#8B4500", "#66CD00"))+
+  scale_x_continuous(breaks = seq(30,90, by=20), limits = c(30,90))+
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + ggtitle("Mean Length") + xlab ("Mean Length")+ ylab ("Sample") # labs(colour="Tax")
+p12
+
+grid.arrange(p11, p12,
+             ncol = 2, nrow = 1, top="Title")
+
+###Supplementary Figures 5
 library(reshape2)
 library(tidyverse)
 library(data.table)
